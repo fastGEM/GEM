1274f7832a62010349e610ba8c753a5426cdd1e5
unknown <HCHEN@SIGN-NB104179.bmsi.a-star.edu.sg>
2016-01-15 19:02:32 +0800

    gem update readme with installation command

--- R/Matrix_eQTL_engine.R
+++ R/Matrix_eQTL_engine.R
@@ -524,6 +524,10 @@ setGeneric("length")
 setMethod("length", "SlicedData",	function(x) {
     return( x$nSlices() );
 })
+setGeneric("FindRow")
+setMethod("FindRow", "SlicedData",	function(x, rn) {
+    return( x$FindRow(rn) );
+})
 setMethod("[[<-", "SlicedData",	function(x,i,value) {
     x$setSlice(i, value);
     return(x);
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 GEM
 ----------------------
+
+
+## Installation
+
+```R
+install.packages("devtools")
+devtools::install_github("fastGEM/GEM")
+```
+
+## Launch the GUI
+
+```R
+GEM_GUI()
+```
